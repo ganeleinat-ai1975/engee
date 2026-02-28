fafcb04e3f7914d7c95f85d664715079853f554d
עינת גן אל <ganel.einat@gmail.com>
2026-02-28 11:51:44 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
+import { base44 } from "@/api/base44Client";
 import { LanguageProvider, useLanguage, t } from "@/components/LanguageProvider";
 import { CartProvider, useCart } from "@/components/CartProvider";
 import {
